ab02bf5406538955c1e1afb3df43d3d2866233a5
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-15 19:52:25 +0900

    Update TodoList.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,23 +1,51 @@
 <script setup lang="ts">
 import ListItem from './ListItem.vue'
-import { ref } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import type { Ref } from 'vue'
 
 type Item = {
   title: string,
   checked?: boolean
 }
-const listItems: Item[] = [
+const listItems: Ref<Item[]> = ref([
   { title: 'Make a todo list app', checked: true },
   { title: 'Predict the weather', checked: false },
   { title: 'Play some tunes', checked: false },
   { title: 'Let\'s get cooking', checked: false },
   { title: 'Pump some iron', checked: false },
   { title: 'Track my expenses', checked: false },
-  { title: 'Organize a game night', checked: false },
+  { title: 'Organise a game night', checked: false },
   { title: 'Learn a new language', checked: false },
   { title: 'Publish my work' }
-]
+])
+
+const updateItem = (item: Item): void => {  const updatedItem = findItemInList(item)
+  if (updatedItem) {
+    toggleItemChecked(updatedItem)
+  }
+}
+const findItemInList = (item: Item): Item | undefined => {
+  return listItems.value.find(
+      (itemInList: Item) => itemInList.title === item.title
+  )
+}
+const toggleItemChecked = (item: Item): void => {
+  item.checked = !item.checked
+}
+
+const sortedList = computed(() => [...listItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0)))
+const setToStorage = (items: Item[]): void => {  localStorage.setItem('list-items', JSON.stringify(items))
+}
+const getFromStorage = (): Item[] | [] => {
+  const stored = localStorage.getItem('list-items')
+  if (stored) {
+    return JSON.parse(stored)
+  }
+  return []
+}
+const storageItems: Ref<Item[]> = ref([])
+
+
 </script>
 
 <template>
@@ -26,7 +54,9 @@ const listItems: Item[] = [
         :key='key'
         v-for='(item, key) in listItems'
     >
-      <ListItem :is-checked='item.checked'>{{ item.title }}</ListItem>
+      <ListItem :is-checked='item.checked' v-on:click.prevent="updateItem(item)">
+        {{ item.title }}
+      </ListItem>
     </li>
   </ul>
 </template>
