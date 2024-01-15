b14ace9a61969c184825fefee9da20ab8cd7a04e
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-15 19:04:41 +0900

    Update TodoList.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,41 @@
 <script setup lang="ts">
 import ListItem from './ListItem.vue'
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+type Item = {
+  title: string,
+  checked?: boolean
+}
+const listItems: Item[] = [
+  { title: 'Make a todo list app', checked: true },
+  { title: 'Predict the weather', checked: false },
+  { title: 'Play some tunes', checked: false },
+  { title: 'Let\'s get cooking', checked: false },
+  { title: 'Pump some iron', checked: false },
+  { title: 'Track my expenses', checked: false },
+  { title: 'Organize a game night', checked: false },
+  { title: 'Learn a new language', checked: false },
+  { title: 'Publish my work' }
+]
 </script>
 
 <template>
   <ul>
-    <ListItem :is-checked="false">This is the slotted content</ListItem>
+    <li
+        :key='key'
+        v-for='(item, key) in listItems'
+    >
+      <ListItem :is-checked='item.checked'>{{ item.title }}</ListItem>
+    </li>
   </ul>
 </template>
 
 <style scoped>
-
+ul {
+  list-style: none;
+}
+li {
+  margin: 0.4rem 0;
+}
 </style>
